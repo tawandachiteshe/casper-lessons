bab1176d5ff6b06bb6ed6927579768ed9116a2d6
Tawanda Chiteshe <dickenstawanda@gmail.com>
2024-12-07 22:37:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+let name = "Tawanda Chiteshe";
+let surname = "Chiteshe"
+let a = 22;
+let isTrue = true;
+let isFalse = false;
+let pi = 3.14;
+let letter = 'T';
+
+
+
+console.log(a);
+console.log(isTrue);
+console.log(isFalse)
+console.log(pi)
+console.log(name);
+console.log(surname); 
+console.log("Hello, Tawanda");
+
+let x = 1 + 1;
+let y = 10 - 3; 
+let z = 20 / 5;
+let j = 15 * 3;
+let i = x - y;
+
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(j);
+console.log(i);
+
+
+
+
+let a1 = 15;
+let a2 = 56;
+let a3 = 77;
+let a4 = 90;
+
+console.log("VERSION 1")
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+let list = [15, 56, 77, 90];
+
+console.log("VERSION 2")
+console.log(list[0]);
+console.log(list[1]);
+console.log(list[2]);
+console.log(list[3]);
+
+console.log("IF STATEMENTS")
+
+let age = 17;
+let age2 = 16;
+
+if(age >= 18 || age2 == 16) {
+    console.log("Eligible");
+} else {
+    console.log("Ineligible")
+}
